48a7e2c3d5d90a57205c91ee3b9ed59c7c6b4fd5
Erik Ness <erikness@nmsu.edu>
2015-02-19 11:38:57 -0700

    Added new definition and functions to the Point class. Not yet complete (as Point is responsible for some heavy lifting), but the skeleton is mostly done.

--- src/edu/nmsu/Point.java
+++ src/edu/nmsu/Point.java
@@ -2,28 +2,96 @@ package edu.nmsu;
 
 /**
  * Created by Erik Ness at 1/21/15 6:02 PM
+ * 
+ * A Point represents two things simultaneously:
+ * - A pixel location relative to the top left corner of the program window
+ * - A coordinate relative to the coordinate system drawn on the program.
+ * 
+ * Behind the scenes, this stores the point's position on the logical grid,
+ * and calculates the pixel coordinates on demand (using the static variables
+ * given to the Point class).
  */
 public class Point
 {
-	public double x;
-	public double y;
+    private static int pixelWidth;
+    private static int pixelHeight;
+    private static double gridXMax;
+    private static double gridXMin;
+    private static double gridYMax;
+    private static double gridYMin;
+    private static Point center;
 
-	public Point(double x, double y)
+    private double gridX;
+    private double gridY;
+
+	/*public Point(double x, double y)
 	{
 		this.x = x;
 		this.y = y;
-	}
+	}*/
 
-    public static Point transform(Point p)
+    private Point() {}  // I may not want a regular constructor...
+
+    public static Point fromGrid(double x, double y)
     {
+        return null;   
+    }
+
+    public static Point fromPixels(double x, double y)
+    {
+        return null;
+    }
+
+
+    /**
+     * For context-free operations where you only need two numbers.
+     */
+    public static class Pair
+    {
+        public double x;
+        public double y;
+        
+        public Pair(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public Pair asGrid()
+    {
+        return Pair(gridX, gridY);
+    }
+
+    public Pair asPixels()
+    {
+        return gridFromPixels();
+    }
+
+    private Pair gridFromPixels()
+    {
+        newX = gridX;
+        newY = gridY;
+        double xScale = pixelWidth / (gridXMax - gridXMin);
+        double yScale = pixelHeight / (gridYMax - gridYMin);
         // Rotate-around-y matrix:
         // [1  0]
         // [0 -1]
+        newX = 1 * newX + 0 * newY;
+        newY = 0 * newX + -1 * newY;
 
         // Scale matrix, where r is pixel length / grid length
-        // [r 0]
-        // [0 r]
+        // [xScale 0]
+        // [0      yScale]
+        newX = xScale * newX + 0 * newY;
+        newX = 0 * newX + yScale * newY;
+        
+        // If we were linear algebra purists, we'd do some added-dimension
+        // transformation in order to translate, but here we'll just add.
+        Pair centerPixels = center.asPixels();
+        newX += centerPixels.x;
+        newY += centerPixels.y;
 
-        return null;
+        return Pair(newX, newY);
     }
 }
