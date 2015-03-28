1d1634dd378ab3be90f7e39146048c3e2bb0fa8b
erik <erikness@nmsu.edu>
2015-03-28 00:25:06 -0600

    Removed DataSetChangeObserver, an old an unnecessary notification scheme.

--- src/edu/nmsu/erikness/pointclickcluster/MetaGraphPanel.java
+++ src/edu/nmsu/erikness/pointclickcluster/MetaGraphPanel.java
@@ -19,7 +19,6 @@ public class MetaGraphPanel extends JPanel
 	private CardLayout layout;
 	// I don't think I can access the Layout's private list of components, but that's why we have a container class!
 	private List<GraphPanel> panels;
-	private List<DataSetChangeObserver> observers;
 	private GraphPanel currentGraphPanel;
 
 	private ToolsPanel toolsPanel;
@@ -28,7 +27,6 @@ public class MetaGraphPanel extends JPanel
 	{
 		layout = new CardLayout();
 		panels = Lists.newArrayList();
-		observers = Lists.newArrayList();
 		this.setLayout(layout);
 	}
 
@@ -43,11 +41,6 @@ public class MetaGraphPanel extends JPanel
 		return panels;
 	}
 
-	public void addDataSetChangeObserver(DataSetChangeObserver obs)
-	{
-		observers.add(obs);
-	}
-
 	public GraphPanel getCurrentGraphPanel()
 	{
 		// for loop is not a big deal, as we only have a couple cards
@@ -57,13 +50,6 @@ public class MetaGraphPanel extends JPanel
 			}
 		}
 		return null;
-	}
-
-	private void updateObservers()
-	{
-		for (DataSetChangeObserver obs : observers) {
-			obs.update(currentGraphPanel);
-		}
 	}
 
 	public void next()
